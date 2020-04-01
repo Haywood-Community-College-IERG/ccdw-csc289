df995bbfbc794ac40c6ed034e1b1be880493f263
David Onder <donder@haywood.edu>
2020-04-01 10:41:07 -0400

    Added new SQL to create the backup tables.

---
+++ sql/RestoreDB2Base_Creation.sql
@@ -0,0 +1,18 @@
+CREATE PROCEDURE RestoreDB2Base_Creation AS 
+BEGIN;
+	/* 
+	EXEC RestoreDB2Base;
+	*/
+	PRINT 'Recreate base_history schema tables';
+	SELECT * INTO base_history.ACAD_PROGRAMS FROM history.ACAD_PROGRAMS;
+	SELECT * INTO base_history.COURSE_SECTIONS FROM history.COURSE_SECTIONS;
+	SELECT * INTO base_history.NON_COURSE FROM history.NON_COURSE;
+	SELECT * INTO base_history.TERMS FROM history.TERMS;
+
+	PRINT 'Recreate base_input schema tables';
+	SELECT * INTO base_input.ACAD_PROGRAMS FROM base_input.ACAD_PROGRAMS;
+	SELECT * INTO base_input.COURSE_SECTIONS FROM input.COURSE_SECTIONS;
+	SELECT * INTO base_input.NON_COURSE FROM input.NON_COURSE;
+	SELECT * INTO base_input.TERMS FROM input.TERMS;
+
+END;
