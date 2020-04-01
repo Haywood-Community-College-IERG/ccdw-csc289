f1e8737972949eecd57b059cb98bfad94649f471
David Onder <donder@haywood.edu>
2020-04-01 10:45:05 -0400

    Removed Procedure and added creation of schemas.

--- sql/RestoreDB2Base_Creation.sql
+++ sql/RestoreDB2Base_Creation.sql
@@ -1,18 +1,16 @@
-CREATE PROCEDURE RestoreDB2Base_Creation AS 
-BEGIN;
-	/* 
-	EXEC RestoreDB2Base;
-	*/
-	PRINT 'Recreate base_history schema tables';
-	SELECT * INTO base_history.ACAD_PROGRAMS FROM history.ACAD_PROGRAMS;
-	SELECT * INTO base_history.COURSE_SECTIONS FROM history.COURSE_SECTIONS;
-	SELECT * INTO base_history.NON_COURSE FROM history.NON_COURSE;
-	SELECT * INTO base_history.TERMS FROM history.TERMS;
+CREATE SCHEMA base_history
+GO                
+CREATE SCHEMA base_input
+GO
 
-	PRINT 'Recreate base_input schema tables';
-	SELECT * INTO base_input.ACAD_PROGRAMS FROM base_input.ACAD_PROGRAMS;
-	SELECT * INTO base_input.COURSE_SECTIONS FROM input.COURSE_SECTIONS;
-	SELECT * INTO base_input.NON_COURSE FROM input.NON_COURSE;
-	SELECT * INTO base_input.TERMS FROM input.TERMS;
+PRINT 'Recreate base_history schema tables';
+SELECT * INTO base_history.ACAD_PROGRAMS FROM history.ACAD_PROGRAMS;
+SELECT * INTO base_history.COURSE_SECTIONS FROM history.COURSE_SECTIONS;
+SELECT * INTO base_history.NON_COURSE FROM history.NON_COURSE;
+SELECT * INTO base_history.TERMS FROM history.TERMS;
 
-END;
+PRINT 'Recreate base_input schema tables';
+SELECT * INTO base_input.ACAD_PROGRAMS FROM base_input.ACAD_PROGRAMS;
+SELECT * INTO base_input.COURSE_SECTIONS FROM input.COURSE_SECTIONS;
+SELECT * INTO base_input.NON_COURSE FROM input.NON_COURSE;
+SELECT * INTO base_input.TERMS FROM input.TERMS;
